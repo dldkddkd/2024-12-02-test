470d05dffeafad171720c600cc828e6b99812104
dldkddkd <jisoolee1997@naver.com>
2024-12-02 10:01:09 +0900

    test

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -1,14 +1,19 @@
+from login import login
+
 import unittest
 
 
 def add_numbers(a, b):
   return a + b
 
-class testModule(unittest,TestCase):
+class testModule(unittest.TestCase):
   def test_add_numbers(self):
     self.assertEqual(add_numbers(2, 5), 7)
     self.assertEqual(add_numbers(1, -1), 0)
     self.assertEqual(add_numbers(2, 2), 5)
+
+def test_login(self):
+  self.assertEqual(login(), 2)
 
 
 if __name__ == '__main__':
