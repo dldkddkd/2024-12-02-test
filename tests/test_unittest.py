93aca5793905affb715b68516718d7b495aa278e
LeeAngAng <37284712+dldkddkd@users.noreply.github.com>
2024-12-02 10:27:10 +0900

    Update test_unittest.py

--- tests/test_unittest.py
+++ tests/test_unittest.py
@@ -10,7 +10,7 @@ class testModule(unittest.TestCase):
   def test_add_numbers(self):
     self.assertEqual(add_numbers(2, 5), 7)
     self.assertEqual(add_numbers(1, -1), 0)
-    self.assertEqual(add_numbers(2, 2), 5)
+    self.assertEqual(add_numbers(2, 2), 4)
 
 def test_login(self):
   self.assertEqual(login(), 2)
